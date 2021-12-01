59abd0be36b76716e039c1171c96f57873a17e69
Bryan`Y. Limz <superuser@brylimz.com>
2021-12-01 19:53:14 +0700

    Fix indentation

--- question_model.py
+++ question_model.py
@@ -1,9 +1,5 @@
 class Question:
-    def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
 
-
-new_q = Question("2+3= 5", "True")
-print(new_q.text)
-print(new_q.answer)
